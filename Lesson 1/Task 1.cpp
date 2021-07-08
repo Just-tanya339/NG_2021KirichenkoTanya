95e646a1f9f2bab3fdf245b7eda16cdfd1665346
Kirichenko Tanya <79713769+Just-tanya339@users.noreply.github.com>
2021-07-08 18:04:31 +0300

    Add files via upload

---
+++ Lesson 1/Task 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Tanya" << endl;
+    cout << "Kirichenko;
+    return 0;
+}
---
+++ Lesson 1/Task 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   cout << "Write the first number:";
+   cin >> a;
+
+   int b;
+   cout << "Write the second number:";
+   cin >> b;
+
+   cout << "Result:";
+    cout << a + b;
+}
