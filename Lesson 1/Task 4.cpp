5ee4d2f85f48fd50f518e9570ef13004b115c481
Kirichenko Tanya <79713769+Just-tanya339@users.noreply.github.com>
2021-07-08 18:30:55 +0300

    Add files via upload

---
+++ Lesson 1/Task 4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int money = 0;
+    cout << "how much did you earn:";
+    cin >> money;
+
+    if (money > 1000)
+  {
+     cout << "Awesome!";
+  }
+
+      if (money < 1000)
+
+  {
+      cout << "so bad!";
+  }
+
+     if(money = 1000000)
+
+   {
+     cout << "you are a millionaire";
+   }
+     cout << ",but work harder";
+
+}
