3a7980ea79618bf337ac6316b0d214e8b55944e8
Kirichenko Tanya <79713769+Just-tanya339@users.noreply.github.com>
2021-07-11 20:13:24 +0300

    Update Task 4.cpp

--- Lesson 1/Task 4.cpp
+++ Lesson 1/Task 4.cpp
@@ -4,26 +4,23 @@ using namespace std;
 
 int main()
 {
+
     int money = 0;
     cout << "how much did you earn:";
     cin >> money;
 
-    if (money > 1000)
-  {
-     cout << "Awesome!";
-  }
+    if (money > 1000) {
+        cout << "Awesome!";
+    }
 
-      if (money < 1000)
+    if (money < 1000){
+        cout << "so bad!";
+    }
 
-  {
-      cout << "so bad!";
-  }
+    if(money < 1000000){
+        cout << "you are a millionaire";
+    }
 
-     if(money = 1000000)
-
-   {
-     cout << "you are a millionaire";
-   }
-     cout << ",but work harder";
+    cout << ",but work harder";
 
 }
